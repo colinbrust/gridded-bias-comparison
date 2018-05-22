de509d9b6c1026fd502349069e0bcfbda1f28ba5
colinbrust <colin.brust@umontana.edu>
2018-05-22 17:00:07 -0600

    changes to density plot function

--- R/makeDenPlots.R
+++ R/makeDenPlots.R
@@ -1,16 +1,45 @@
 
+makePlot <- function(variable, time, stat, ...) {
 
-  if(time == "Month") {
-    plotTitle <- tools::toTitleCase(paste("Density Plot of", variable, plotDat$Statistic[1], "by", time))
-  } else if (time == "Season") {
-    plotTitle <- tools::toTitleCase(paste("Density Plot of", variable, plotDat$Statistic[1], "by", time))
-  } else if (time == "Year") {
-    plotTitle <- tools::toTitleCase(paste("Density Plot of 30-Year", variable, plotDat$Statistic[1]))
+  #library(feather)
+  library(tibble)
+  library(dplyr)
+  library(ggplot2)
+
+
+  tmpPallete <- c("#5B1A18", "#D67236", "#FD6467", "#F1BB7B")
+  pptPallete <- c("#D8A499", "#5B1A18", "#D67236", "#FD6467")
+
+  toSubset = c(...)
+
+  dat <- "./analysis/data/derived_data/files/" %>%
+    paste0(time)%>%
+    paste(variable, paste0(stat, ".feather"), sep = "_") %>%
+    feather::read_feather() %>%
+    dplyr::filter_(toSubset)
+
+  dat$Dataset <- factor(dat$Dataset)
+
+  if(time == "Monthly") {
+
+    plotTitle <- tools::toTitleCase(paste("Boxplot of", variable, dat$Statistic[1], "by", time, toString(toSubset)))
+    dat$Index <- factor(dat$Index)
+    levels(dat$Index) <- month.name
+
+  } else if (time == "Seasonal") {
+
+    plotTitle <- tools::toTitleCase(paste("Boxplot of", variable, dat$Statistic[1], "by", time,  toString(toSubset)))
+    dat$Index <- factor(dat$Index)
+    levels(dat$Index) <-  c("Winter", "Spring", "Summer", "Autumn")
+
+  } else if (time == "Annual") {
+
+    plotTitle <- tools::toTitleCase(paste("Boxplot of 30-Year", variable, dat$Statistic[1]),  toString(toSubset))
   }
 
   den <-  ggplot2::ggplot(plotDat, aes(x = Value,  color = Dataset)) +
     geom_density() +
-    facet_wrap(~Time)
+    facet_wrap(~Index)
 
   if(variable == "tmax" || variable == "tmin") {
     den+scale_color_manual(values=tmpPallete)  +
@@ -31,3 +60,8 @@
             legend.title = element_text(hjust = 0.5, colour="gray15", face = "bold"),
             legend.text = element_text(colour="gray26", face = "bold"))
   }
+
+
+
+}
+
